da5fccdb87d481fad7645648e0fdf8822e17f0e0
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2024-03-17 15:06:26 +0200

    Initial commit

---
+++ src/main/java/bg/wardrobe/wardrobe/WardrobeApplication.java
@@ -0,0 +1,13 @@
+package bg.wardrobe.wardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardrobeApplication.class, args);
+	}
+
+}
