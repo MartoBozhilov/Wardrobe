cba48259ee906bbb04066a76c3713e68c57276fa
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2024-03-22 16:45:46 +0200

    Build a repositories.

---
+++ src/main/java/bg/wardrobe/wardrobe/repository/CartItemRepository.java
@@ -0,0 +1,10 @@
+package bg.wardrobe.wardrobe.repository;
+
+import bg.wardrobe.wardrobe.model.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+}
---
+++ src/main/java/bg/wardrobe/wardrobe/repository/OrderDetailRepository.java
@@ -0,0 +1,10 @@
+package bg.wardrobe.wardrobe.repository;
+
+import bg.wardrobe.wardrobe.model.entity.OrderDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+
+}
