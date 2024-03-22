f1282d1cae09f26f3a2c040e1f88e26dccb342fc
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2024-03-22 17:51:35 +0200

    Thymeleaf needed css and js files.

--- src/main/java/bg/wardrobe/wardrobe/controller/HomeController.java
+++ src/main/java/bg/wardrobe/wardrobe/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     public String Test() {
         return "index";
     }
