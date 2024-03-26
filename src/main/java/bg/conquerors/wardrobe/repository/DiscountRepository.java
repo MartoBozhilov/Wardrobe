1414be82f76e6b6b337a12e5d29315ce63bd4e3c
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2024-03-26 18:53:57 +0200

    Create DiscountRepository.java

---
+++ src/main/java/bg/conquerors/wardrobe/repository/DiscountRepository.java
@@ -0,0 +1,10 @@
+package bg.conquerors.wardrobe.repository;
+
+import bg.conquerors.wardrobe.model.entity.Discount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscountRepository extends JpaRepository<Discount, Long> {
+    
+}
