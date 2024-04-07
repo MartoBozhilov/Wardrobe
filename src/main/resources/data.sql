68e00c07aa9cfeecf121099bf1241e3fae9d1ce9
KristianTV <75727323+KristianTV@users.noreply.github.com>
2024-04-07 19:39:40 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,8 @@ VALUES
     (1, 'admin@test.com', 'Admin', 'Adminov', 'a88d78aa482512691254a2177fc435ac9d6f6a62a7d12da7fcf2560f9333c5a36c859249a9fd5ea3a551b9f0cc3430af', '0899445566', 0, 'admin4eto'),
     (2, 'user@test.com', 'User', 'Userov', '65596bc1a3e2e3060b10064988170d2733b914e0d1bfcacaa46f5584a3489eee71d0aeaa383817575431e81a5308a234', '0899443322', 0, 'user4eto');
 
+-- admin4eto (admin1234)
+-- user4eto (user1234)
 
 INSERT INTO roles (`id`, `role`)
 VALUES
