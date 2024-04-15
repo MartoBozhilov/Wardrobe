7e8c98a9899d5b7eb6161e42590e831d7a6aa2e2
Krasi <kkrrasii@gmail.com>
2024-04-15 17:28:50 +0300

    admin page design update
    трябва да се направи дизайн за edit и add new product страниците

--- src/main/java/bg/conquerors/wardrobe/controller/AdminController.java
+++ src/main/java/bg/conquerors/wardrobe/controller/AdminController.java
@@ -14,6 +14,7 @@ import lombok.Data;
 import org.hibernate.annotations.processing.Find;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -42,8 +43,8 @@ public class AdminController {
      * */
 
     @GetMapping("")
-    public String admin() {
-
+    public String admin(Model model) {
+        model.addAttribute("products",productService.getViewOfProducts());
         return "admin/admin";
     }
 
