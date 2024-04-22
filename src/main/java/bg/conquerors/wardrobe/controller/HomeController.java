fe73a7087c188e8b6bc629ac4449aaa2710a76c8
MartoBozhilov <150125921+MartoBozhilov@users.noreply.github.com>
2024-04-22 11:08:32 +0300

    Clean HomeController

--- src/main/java/bg/conquerors/wardrobe/controller/HomeController.java
+++ src/main/java/bg/conquerors/wardrobe/controller/HomeController.java
@@ -1,44 +1,23 @@
 package bg.conquerors.wardrobe.controller;
 
-import bg.conquerors.wardrobe.model.dto.AddProductDTO;
-import bg.conquerors.wardrobe.model.enums.SizeEnum;
-import bg.conquerors.wardrobe.service.AdminService;
-import bg.conquerors.wardrobe.service.ProductService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
-    private final ProductService productService;
-    private final AdminService adminService;
-    //private static final Logger log = LoggerFactory.getLogger(HomeController.class);
-
-    public HomeController(AdminService adminService, ProductService productService) {
-        this.adminService = adminService;
-        this.productService = productService;
-    }
 
     @GetMapping("/")
-    public String Test10() {
+    public String home() {
 
         return "index";
     }
 
-    @GetMapping("/about")
-    public String Test() {
-
-        return "about";
-    }
     @GetMapping("/faq")
     public String faqPage() {
 
         return "faq";
     }
+
     @GetMapping("/buy-policy")
     public String buyPolicy() {
 
@@ -50,30 +29,11 @@ public class HomeController {
 
         return "contact";
     }
+
     @GetMapping("/index")
     public String homeIndex() {
 
         return "index";
-    }
-
-    @GetMapping("/product")
-    public String Test6() {
-
-        return "product";
-    }
-
-//    @GetMapping("/product-detail/{id}")
-//    public String productDetailWithId(@PathVariable("id") Long id, Model model) {
-//        model.addAttribute("product", adminService.getProductById(id));
-//        model.addAttribute("itemId", id);
-//        model.addAttribute("sizeOptions", SizeEnum.values());
-//        return "product-detail";
-//    }
-
-    @GetMapping("/product-detail")
-    public String productDetail() {
-
-        return "product-detail";
     }
 
 }
